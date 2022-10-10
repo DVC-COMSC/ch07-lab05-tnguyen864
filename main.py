24052604b062faf52b267af11357711c9dcaf132
Thuy Nguyen <thuicide@gmail.com>
2022-10-10 02:55:40 -0700

    Chap7 Lab5

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-
-# ******************************
-# Make your Code
-# ******************************
+# Take 5 values in one string and split values by space
+print("Enter 5 integer values as one string:")
 strval = input().split()
-numbers = []
+numbers = []		# List for numbers
 for v in strval:
-	numbers.append(int(v))
-print (numbers)
-# the below llin 11 are same as the lines from 5 to 8
-# numbers = list(map(int, strval))
-# print (numbers)
+	numbers.append(int(v))		# Convert string to int and append to list
+
+# Get value to count occurrences of
+search = int(input("Enter an integer value to search for: "))
+
+# Count number of occurences of number given
+found = numbers.count(search)
+
+# Print list and occurences
+print("List:", numbers)
+print("The occurrences of", search, "=", found)
